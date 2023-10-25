06780e28e598b2a3174260c81690129b1ba2c5b3
Sobin <sobingeneral@gmail.com>
2023-10-25 22:45:06 +0700

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Nav from "./components/Nav";
+
+function App() {
+  return <Nav />;
+}
+
+export default App;
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <div className="flex flex-col items-center bg-orange-200 h-screen w-1/6 shadow-md rounded-md">
+      <h1 className="text-3xl mx-auto my-5 text-bold font-bold">SHOP LIST</h1>
+      <button className="btn w-3/4">+ Add new shop</button>
+    </div>
+  );
+};
+
+export default Nav;
