3c2e200b8d011701e0d4a1c4e9a6d7716714c4f8
Sobin <sobingeneral@gmail.com>
2023-10-26 00:59:31 +0700

    feat: add new shop

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,8 +1,63 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
 const Nav = () => {
+  const [modalState, setModalState] = useState(false);
+  const [shopName, setShopName] = useState("");
+  const [shopLatitude, setShopLatitude] = useState("");
+  const [shopLongtitude, setShopLongtitude] = useState("");
+  const [shopUrl, setShopUrl] = useState("");
+  const handleChange = (e) => {};
   return (
-    <div className="flex flex-col items-center bg-orange-200 h-screen w-1/6 shadow-md rounded-md">
-      <h1 className="text-3xl mx-auto my-5 text-bold font-bold">SHOP LIST</h1>
-      <button className="btn w-3/4">+ Add new shop</button>
+    <div>
+      <nav className="flex flex-col items-center bg-slate-200 h-screen w-1/6 shadow-md rounded-md m-2">
+        <h1 className="text-3xl mx-auto my-5 text-bold font-bold">SHOP LIST</h1>
+        <button onClick={() => setModalState(true)} className="btn w-3/4">
+          + Add new shop
+        </button>
+      </nav>
+      <Modal modalState={modalState}>
+        <form className="flex flex-col gap-3">
+          <h3 className="text-lg font-bold">Add New Shop</h3>
+          <input
+            type="text"
+            value={shopName}
+            onChange={(e) => setShopName(e.target.value)}
+            placeholder="name"
+            className="input w-full border-black"
+          />
+          <input
+            type="number"
+            value={shopLatitude}
+            onChange={(e) => setShopLatitude(e.target.value)}
+            placeholder="latitude"
+            className="input w-full border-black"
+          />
+          <input
+            type="number"
+            value={shopLongtitude}
+            onChange={(e) => setShopLongtitude(e.target.value)}
+            placeholder="longtitude"
+            className="input w-full border-black"
+          />
+          <input
+            type="text"
+            value={shopUrl}
+            onChange={(e) => setShopUrl(e.target.value)}
+            placeholder="image url"
+            className="input w-full border-black"
+          />
+        </form>
+        <div className="flex mt-3 justify-between ">
+          <button className="btn bg-blue-500 w-28">Save</button>
+          <button
+            onClick={() => setModalState(false)}
+            className="btn bg-red-500 w-28"
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
